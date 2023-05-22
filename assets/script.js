496846eb07e03d1bd6148dbbebc2bc98f4995e0e
Mark T <aydenthos@gmail.com>
2023-05-22 08:56:31 -0700

    building variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+var startButton = document.getElementById ("start-button");
+var questionContainer = document.getElementById ("question-container");
+var answerContainer = document.getElementById ("answer-container");
+var timeContainer = document.getElementById ("time-container");
+var scoreContainer = document.getElementById ("score-container");
+var questions = [
+    {
+        question: "What character do you use to create an in-line comment?",
+        answers: [""]
+    }
+]
 
+startButton.addEventListener("click", function(){});
