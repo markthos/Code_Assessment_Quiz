db7c39c5486e883c97c48939589a6fbe68cfb102
Mark T <aydenthos@gmail.com>
2023-05-23 07:34:27 -0700

    added a submit button and started fleshing out the questions

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,14 @@ var timeContainer = document.getElementById ("time-container");
 var scoreContainer = document.getElementById ("score-container");
 var questions = [
     {
-        question: "What character do you use to create an in-line comment?",
-        answers: [""]
+        question: "What characters do you use to create an in-line comment?",
+        answers: ["//", "n/", "/**/"],
+        correctAnswer: "//"
+    },
+    {
+        question: "Which code is a Jquery operation?",
+        answers: ['document.queryselectAll("#root")', '$("#root")', 'document.getElementById("#root")'],
+        coorectAnswer: '$("#root")'
     }
 ]
 
