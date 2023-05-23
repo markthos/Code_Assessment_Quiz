282acc35d321595e9c8a921ef4b46f174d337c6b
Mark T <aydenthos@gmail.com>
2023-05-23 09:25:48 -0700

    fixed syntax error

--- assets/script.js
+++ assets/script.js
@@ -13,17 +13,18 @@ var questions = [
         question: 'Which code is a Jquery operation?',
         answers: ['document.queryselectAll("#root")', '$("#root")', 'document.getElementById("#root")'],
         coorectAnswer: '$("#root")'
-    }
+    },
     {
         question: 'Which of these uses LIFO to access, store, and retrieve data?',
         answers: ['Queue', 'Array', 'Stack'],
         correctAnswer: 'Stack'
-    }
+    },
     {
         question: 'Which of these are a non-linear data structure?',
         answers: ['Trees', 'Arrays', 'Stacks'],
         correctAnswer: 'Trees'
-    }{
+    },
+    {
         question: 'In OOPS which concept refers to the wrapping of code and data together into a single unit',
         answers: ['Inheritance', 'Encapsulation', 'Polymorphism'],
         correctAnswer: 'Encapsulation'
