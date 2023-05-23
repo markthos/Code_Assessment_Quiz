03a0ebc121a3d77cd99a1da898bae85cb8721cec
Mark T <aydenthos@gmail.com>
2023-05-23 10:54:09 -0700

    add function to show wuestions with answer field

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
+
+//pull html elements into usable variables
 var startButton = document.getElementById ("start-button");
 var questionContainer = document.getElementById ("question-container");
-var answerContainer = document.getElementById ("answer-container");
 var timeContainer = document.getElementById ("time-container");
 var scoreContainer = document.getElementById ("score-container");
+var submitButton =document.getElementById("submit-button");
+
+//create question objects with content, answers, and correct answers for quiz
 var questions = [
     {
         question: 'What characters do you use to create an in-line comment?',
@@ -12,7 +16,7 @@ var questions = [
     {
         question: 'Which code is a Jquery operation?',
         answers: ['document.queryselectAll("#root")', '$("#root")', 'document.getElementById("#root")'],
-        coorectAnswer: '$("#root")'
+        correctAnswer: '$("#root")'
     },
     {
         question: 'Which of these uses LIFO to access, store, and retrieve data?',
@@ -30,5 +34,32 @@ var questions = [
         correctAnswer: 'Encapsulation'
     }
 ]
+//declare variables for quiz functions
+var currentQuestion = 0;
+var timer = 0;
 
-startButton.addEventListener("click", function(){});
+//create function to start quiz with timer at 60 seconds
+function startQuiz() {
+    timer = 60;
+    console.log(timer);
+    showQuestion();
+}
+
+//create function to show questions with answer field
+function showQuestion() {
+    var question = questions[currentQuestion];
+    document.getElementById("question-container").innerHTML = question.question;
+    for (var i = 0; i < question.answers.length; i++) {
+        var answer = document.createElement("input");
+        answer.type = "radio";
+        answer.name = "question";
+        answer.value = question.answers[i];
+        answer.id = "answer" + i;
+        document.getElementById("question-container").appendChild(answer); 
+    }
+}
+
+//function submitAnswer(){}
+
+startButton.addEventListener("click", startQuiz);
+//submitButton.addEventListener("click", submitAnswer);
