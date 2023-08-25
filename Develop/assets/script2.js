f0ea4b70229840ed433ef4eec6f301da165559f6
Mark T <aydenthos@gmail.com>
2023-08-24 17:54:32 -0700

    working on logic for quiz

--- Develop/assets/script2.js
+++ Develop/assets/script2.js
@@ -1,73 +1,72 @@
 const quizQuestions = [
     {
         question: "What is the HTML tag under which one can write the JavaScript code?",
-        answers: {
-            a: "<javascript>",
-            b: "<scripted>",
-            c: "<script>",
-            d: "<js>"
-        },
-        correctAnswer: "c"
+        answers: [
+            "<javascript>",
+            "<scripted>",
+            "<script>",
+            "<js>"
+        ],
+        correctAnswer: "<script>"
     },
     {
         question: "Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?",
-        answers: {
-            a: "alertbox(“GeeksforGeeks”);",
-            b: "msg(“GeeksforGeeks”);",
-            c: "msgbox(“GeeksforGeeks”);",
-            d: "alert(“GeeksforGeeks”);"
-        },
-        correctAnswer: "d"
+        answers: [
+            "alertbox(“GeeksforGeeks”);",
+            "msg(“GeeksforGeeks”);",
+            "msgbox(“GeeksforGeeks”);",
+            "alert(“GeeksforGeeks”);"
+        ],
+        correctAnswer: "alert(“GeeksforGeeks”);"
     },
     {
         question: "What is the correct syntax for referring to an external script called “geek.js”?",
-        answers: {
-            a: "<script src=”geek.js”>",
-            b: "<script href=”geek.js”>",
-            c: "<script ref=”geek.js”>",
-            d: "<script name=”geek.js”>"
-        },
-        correctAnswer: "a"
+        answers: [
+            "<script src=”geek.js”>",
+            "<script href=”geek.js”>",
+            "<script ref=”geek.js”>",
+            "<script name=”geek.js”>"
+        ],
+        correctAnswer: "<script src=”geek.js”>"
     },
     {
         question: "Which of the following is not a reserved word in JavaScript?",
-        answers: {
-            a: "interface",
-            b: "throws",
-            c: "program",
-            d: "short"
-        },
-        correctAnswer: "c"
+        answers: [
+            "interface",
+            "throws",
+            "program",
+            "short"
+        ],
+        correctAnswer: "program"
     }
 ];
 
-// Get the start button, questions form, highscore value, highscore initials, timer value
-const startButton = document.getElementById("start-button");
-const questionsForm = document.getElementById("questions-form");
-const highscoreValue = document.getElementById("highscore-value");
-const highscoreInitials = document.getElementById("highscore-initials");
-const timerValue = document.getElementById("timer-value");
+     
+
+const startButton = document.getElementById("startBtn");
+const questionForm = document.getElementById("questionForm");
+const questionElement = document.getElementById("question");
+const answerElements = document.querySelectorAll("span[id^='answer']");
+const highscoreValue = document.getElementById("highscoreValue");
+const highscoreInitials = document.getElementById("highscoreInitials");
+const timerValue = document.getElementById("timerValue");
 
 let currentQuestionIndex = 0;
-let timer;
 let timeLeft = 60;
+let timerInterval;
 let score = 0;
 
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    // Hide the start button
     startButton.style.display = "none";
-    // Show the questions form
-    questionsForm.style.display = "block";
-    // Start the timer
+    questionForm.style.display = "block";
     startTimer();
-    // Show the first question
     showQuestion(currentQuestionIndex);
 }
 
 function startTimer() {
-    timer = setInterval(function () {
+    timer = setInterval(function() {
         timeLeft--;
         timerValue.textContent = timeLeft;
         if (timeLeft <= 0) {
@@ -75,3 +74,61 @@ function startTimer() {
         }
     }, 1000);
 }
+
+function showQuestion(index) {
+    if (index < quizQuestions.length) {
+        const questionData = quizQuestions[index];
+        questionElement.textContent = questionData.question;
+        answerElements.forEach((answerElement, i) => {
+            answerElement.textContent = questionData.answers[i];
+        });
+    } else {
+        endQuiz();
+    }
+}
+
+function checkAnswer(selectedAnswer) {
+    const correctAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+    if (selectedAnswer === correctAnswer) {
+        score++;
+    } else {
+        timeLeft -= 10; // Penalty for incorrect answer
+    }
+    currentQuestionIndex++;
+    showQuestion(currentQuestionIndex);
+}
+
+function endQuiz() {
+    clearInterval(timer);
+    questionForm.style.display = "none";
+    highscoreValue.textContent = score;
+    highscoreInitials.textContent = "-";
+    // Display input field for initials and save button
+    const initialsInput = document.createElement("input");
+    initialsInput.placeholder = "Enter your initials";
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+    saveButton.addEventListener("click", function() {
+        const initials = initialsInput.value;
+        if (initials) {
+            saveHighscore(initials, score);
+        }
+    });
+    document.getElementById("quiz").appendChild(initialsInput);
+    document.getElementById("quiz").appendChild(saveButton);
+}
+
+function saveHighscore(initials, score) {
+    // Implement local storage logic here
+    
+}
+
+function loadHighscore() {
+    // Implement local storage logic here to load highscore
+    // Update highscoreValue and highscoreInitials based on highscoreData
+}
+
+// Load highscore when the page loads
+window.addEventListener("load", function() {
+    loadHighscore();
+});
